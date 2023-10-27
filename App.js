fd83555f2e390e4981a4841add6ad7afe2ff82fe
ankitasharma1999 <98891495+ankitasharma1999@users.noreply.github.com>
2023-10-27 12:57:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import Comp from "./form";
+
+//import Counter from './counter';
+//import Display from './parentdis';
+
+/*function App() {
+  const [count, setCount] = useState(0);*/
+  function App() {
+  return (
+    <div>
+      {/*<Counter count={count} setCount={setCount} />
+      <Display count={count} />*/}
+      <Comp/>
+    </div>
+    )
+    }
+
+
+export default App;
